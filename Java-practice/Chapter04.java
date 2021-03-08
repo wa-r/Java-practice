55961f88f045d951b849f08ba2878c7a5d0d2b69
wa-r <nyuuuuun14@gmail.com>
2021-03-08 20:00:18 +0900

    [追加] Chapter04.java

---
+++ Java-practice/Chapter04.java
@@ -0,0 +1,11 @@
+public class Chapter04 {
+
+	public static void main (String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str1 = "Hanako";
+		System.out.println(str1);
+		str1 = "ありがとう";
+		System.out.println(str1);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
